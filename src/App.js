02553c788de637f6acae2b02146f6ced557eb889
simon <nikolasimonovic94@gmail.com>
2021-02-12 18:28:59 +0100

    😀

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const Person = ({ img, name, job }) => {
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="person">
+      <img src={url} alt="" />
+      <div>
+        <h4>{name}</h4>
+        <h4>{job}</h4>
+      </div>
     </div>
   );
-}
+};
+
+const PersonList = () => {
+  const people = [
+    {
+      img: 22,
+      name: "John",
+      job: "Developer",
+    },
+    {
+      img: 32,
+      name: "Bob",
+      job: "Designer",
+    },
+    {
+      img: 52,
+      name: "Peter",
+      job: "Artist",
+    },
+  ];
+  return people.map(({ img, name, job }) => <Person img={img} name={name} job={job} />);
+};
+
+const App = () => {
+  return <PersonList />;
+};
 
 export default App;
